9bd5410656636d10ba483e82b7ead216a75fc8bb
MoroM77 <MoroM77@gmail.com>
2019-10-01 22:24:45 +0200

    Aggiunte views, view di layout e start

--- src/myCourse/Controllers/HomeController.cs
+++ src/myCourse/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace myCourse.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Sono la index della Home");
+            return View();
         }
     }
 }
